c8c147a19bd80f46bf85ed31842cee91879144f4
ThanhLong <thanhlong0702aa@gmail.com>
2023-07-15 22:03:16 +0700

    long

--- app/src/main/java/com/example/mynotes/SplashActivity.java
+++ app/src/main/java/com/example/mynotes/SplashActivity.java
@@ -21,7 +21,7 @@ public class SplashActivity extends AppCompatActivity {
             public void run() {
                 FirebaseUser currentUser = FirebaseAuth.getInstance().getCurrentUser();
                 if(currentUser==null){
-                    startActivity(new Intent(SplashActivity.this,Login.class));
+                    startActivity(new Intent(SplashActivity.this,LoginActivity.class));
                 }else{
                     startActivity(new Intent(SplashActivity.this,MainActivity.class));
                 }
