cffcf306ee8579b7087e2fb01e5c47442e8c7051
hien <hientdhe163020@gmail.com>
2023-07-16 17:31:29 +0700

    add change password, update note, delete note

--- app/src/main/java/com/example/mynotes/NoteDetailsActivity.java
+++ app/src/main/java/com/example/mynotes/NoteDetailsActivity.java
@@ -11,6 +11,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.TextView;
 
 import com.google.firebase.Timestamp;
 import com.google.firebase.firestore.DocumentReference;
@@ -19,7 +20,9 @@ public class NoteDetailsActivity extends AppCompatActivity {
 
     EditText titleEditText, contentEditText;
     ImageButton saveNoteBtn;
-
+    TextView pageTitle,deleteNote;
+    String title,content,docId;
+    boolean isUpdateNote = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,6 +32,23 @@ public class NoteDetailsActivity extends AppCompatActivity {
         titleEditText = findViewById(R.id.notes_title_text);
         contentEditText = findViewById(R.id.notes_content_text);
         saveNoteBtn = findViewById(R.id.save_note_btn);
+        pageTitle = findViewById(R.id.page_title);
+        deleteNote = findViewById(R.id.delete_note);
+        //get data from intent
+        title = getIntent().getStringExtra("title");
+        content = getIntent().getStringExtra("content");
+        docId = getIntent().getStringExtra("docId");
+        //check if data is null
+        if(docId!=null && !docId.isEmpty()){
+            isUpdateNote = true;
+            titleEditText.setText(title);
+            contentEditText.setText(content);
+        }
+        //change title if not add new
+        if (isUpdateNote){
+            pageTitle.setText("Edit your note");
+            deleteNote.setVisibility(View.VISIBLE);
+        }
         saveNoteBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -47,9 +67,12 @@ public class NoteDetailsActivity extends AppCompatActivity {
             }
         });
 //        saveNoteBtn.setOnClickListener((v -> saveNote()));
+        deleteNote.setOnClickListener((v -> deleteNoteInFireBase()));
     }
 
-//    void saveNote(){
+
+
+    //    void saveNote(){
 //        String noteTitle = titleEditText.getText().toString();
 //        String noteContent = contentEditText.getText().toString();
 //        if (noteTitle.trim().isEmpty()) {
@@ -65,16 +88,42 @@ public class NoteDetailsActivity extends AppCompatActivity {
 //    }
     void saveNoteToFireBase(Note note) {
         DocumentReference documentReference;
-        documentReference = Utility.getCollectionReferenceForNotes().document();
+        //check if update mode
+        if(isUpdateNote == true){
+            documentReference = Utility.getCollectionReferenceForNotes().document(docId);
+        } else {
+            documentReference = Utility.getCollectionReferenceForNotes().document();
+        }
+
         documentReference.set(note).addOnCompleteListener(new OnCompleteListener<Void>() {
             @Override
             public void onComplete(@NonNull Task<Void> task) {
                 if (task.isSuccessful()) {
                     // note is added
-                    Utility.showToast(NoteDetailsActivity.this, "Note added successfully!");
+                    if(isUpdateNote == true){
+                        Utility.showToast(NoteDetailsActivity.this, "Note update successfully!");
+                    } else {
+                        Utility.showToast(NoteDetailsActivity.this, "Note added successfully!");
+                    }
                     finish();
                 } else {
                     Utility.showToast(NoteDetailsActivity.this, "Failed while adding note!");
+                }
+            }
+        });
+    }
+    private void deleteNoteInFireBase() {
+        DocumentReference documentReference;
+            documentReference = Utility.getCollectionReferenceForNotes().document(docId);
+        documentReference.delete().addOnCompleteListener(new OnCompleteListener<Void>() {
+            @Override
+            public void onComplete(@NonNull Task<Void> task) {
+                //check if suscess
+                if (task.isSuccessful()) {
+                    Utility.showToast(NoteDetailsActivity.this, "Note is deleted");
+                    finish();
+                } else {
+                    Utility.showToast(NoteDetailsActivity.this, "Failed to delete");
                 }
             }
         });
